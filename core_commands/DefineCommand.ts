4d36573f172eec19be3f010cfe7cde1d8b192227
Curt Cox <curtcox@gmail.com>
2024-05-06 15:32:50 -0500

    Implementing define command.

--- core_commands/DefineCommand.ts
+++ core_commands/DefineCommand.ts
@@ -1,23 +1,60 @@
 import { CommandDefinition, CommandContext, CommandData } from "../CommandDefinition.ts";
+import { use } from "../ToolsForCommandWriters.ts";
 
 const meta = {
     name: "define",
     doc: "define a command",
     source: import.meta.url,
-    input_formats: ["text"],
-    output_formats: ["text"],
+    input_formats: ["URL","text/javascript","text/typescript"],
+    output_formats: ["CommandDefinition"],
 };
 
+async function define_from_module(module:any): Promise<CommandDefinition> {
+    if (!module) throw new Error("no module found");
+    if (module.command) return module.command;
+    if (module.meta && module.func) return { meta: module.meta, func: module.func };
+    console.log({module});
+    throw new Error(`No command found in module ${module}`);
+}
+
+async function define_from_url(url: string): Promise<CommandDefinition> {
+    const module = await import(url);
+    return define_from_module(module);
+}
+
+async function define_from_javascript(text: string): Promise<CommandDefinition> {
+    const blob = new Blob([text], { type: 'application/javascript' });
+    const url = URL.createObjectURL(blob);
+    return define_from_url(url);
+}
+
+async function define_from_typescript(text: string): Promise<CommandDefinition> {
+    const blob = new Blob([text], { type: 'application/typescript' });
+    const url = URL.createObjectURL(blob);
+    return define_from_url(url);
+}
+
+async function define(data: CommandData): Promise<CommandDefinition> {
+    const format = data.format;
+    if (format === "URL")             return define_from_url(data.content);
+    if (format === "text/javascript") return define_from_javascript(data.content);
+    if (format === "text/typescript") return define_from_typescript(data.content);
+    throw new Error("unsupported format");
+}
+
+const func = async (context: CommandContext, data: CommandData) => {
+    const command = await define(data);
+    return Promise.resolve({
+           commands: use(command, context.commands),
+           output: {
+               format: "CommandDefinition",
+               content: command
+           }
+        }
+    );
+}
+
+
 export const define_cmd: CommandDefinition = {
-    meta,
-    func: async (context: CommandContext, _options: CommandData) => {
-        return Promise.resolve({
-             commands: context.commands,
-             output: {
-                 format: "text",
-                 content: "defined"
-             }
-            }
-        );
-    },
+    meta, func
 };
--- core_commands/DefineCommand_test.ts
+++ core_commands/DefineCommand_test.ts
@@ -1,9 +1,13 @@
-import { assertEquals } from "https://deno.land/std/assert/mod.ts";
-import { CommandContext, CommandDefinition } from "../CommandDefinition.ts";
+import { assertEquals } from "https://deno.land/std@0.223.0/assert/mod.ts";
+import { CommandContext, CommandData, CommandDefinition } from "../CommandDefinition.ts";
 import { define_cmd } from "./DefineCommand.ts";
 import { nop_cmd } from "./NopCommand.ts";
 
 const empty_commands: Record<string, CommandDefinition> = {};
+const empty_data: CommandData = {
+  format: "text",
+  content: ""
+}
 
 const empty_context: CommandContext = {
   commands: empty_commands,
@@ -23,64 +27,187 @@ const context_with_func = (content: string) => ({
   }
 });
 
-const options = (options: string) => ({
-  format: "options",
-  content: options
+const input = (data: CommandData) => ({
+  commands: empty_commands,
+  previous: nop_cmd,
+  input: data
+})
+
+const javascript = (content: string) => ({
+  format: "text/javascript",
+  content: content
 });
 
-Deno.test("define function from options using arrow function", async () => {
-    const f = "add: (x: number, y: number) => x + y";
-    const defined = await define_cmd.func(empty_context, f);
-    assertEquals(defined.output.content, "defined add");
-
-    const commands = defined.commands;
-    const sum = await commands["add"].func(empty_context, options("--x=10 --y=2"));
-    assertEquals(sum.output.content, "12");
+const typescript = (content: string) => ({
+  format: "text/typescript",
+  content: content
 });
 
-Deno.test("define function from options using function declaration", async () => {
-    const f = "function cat(p1, p2) { return p1 + p2; }";
-    const defined = await define_cmd.func(empty_context, options(f));
-    assertEquals(defined.output.content, "defined cat");
-
-    const commands = defined.commands;
-    const cat = await commands["cat"].func(empty_context, options("--p1=Hey --p2=You"));
-    assertEquals(cat.output.content, "HeyYou");
-});
-
-Deno.test("define function from options using function expression", async () => {
-    const f = "const rgb = function(r, g, b) { return `rgb(${r},${g},${b})`; }";
-    const defined = await define_cmd.func(empty_context, f);
-    assertEquals(defined.output.content, "defined rbg");
-
-    const commands = defined.commands;
-    const rgb = await commands["rgb"].func(empty_context, options("--r=10 --g=4 --b=2"));
-    assertEquals(rgb.output.content, "rgb(10,4,2)");
-});
-
-Deno.test("define function from input using arrow function", async () => {
-    const f = "multiply: (a: number, b: number) => a * b";
-    const defined = await define_cmd.func(context_with_func(f), options(""));
-    assertEquals(defined.output.content, "defined multiply");
-
-    const commands = defined.commands;
-    const multiply = await commands["multiply"].func(empty_context, options("--a=10 --b=2"));
-    assertEquals(multiply.output.content, "20");
-});
-
-Deno.test("define function from input using function declaration", async () => {
-    const f = `
-    function dup(times: number) {
-        let out = "";
-        for (let i = 0; i < times; i++) {
-            out += input;
+Deno.test("define function from full JavaScript definition", async () => {
+    const defined = await define_cmd.func(empty_context, javascript(
+      `export const command = {
+        meta: {
+          name: "nop",
+          doc: "javascript nop",
+          source: import.meta.url,
+          input_formats: [],
+          output_formats: []
+        },
+        func: (context, _options) => {
+          return Promise.resolve({
+            commands: context.commands,
+            output: context.input
+          });
         }
-        return out;
-    }`;
-    const defined = await define_cmd.func(context_with_func(f), options(""));
-    assertEquals(defined.output.content, "dup");
+      };
+    `));
+    const out = defined.output.content.meta;
+    assertEquals(out.name, "nop");
+    assertEquals(out.input_formats, []);
+    assertEquals(out.output_formats, []);
 
     const commands = defined.commands;
-    const duped = await commands["dup"].func(context_with_func("root"), options("3"));
-    assertEquals(duped.output.content, "rootrootroot");
+    const nop = commands["nop"];
+    const meta = nop.meta;
+    assertEquals(meta.name, "nop");
+    assertEquals(meta.doc, "javascript nop");
+    assertEquals(meta.input_formats, []);
+    assertEquals(meta.output_formats, []);
+
+    const data1 = {format: "URL", content: new URL(import.meta.url)};
+    const result1 = await nop.func(input(data1), empty_data);
+    assertEquals(result1.output, data1);
+
+    const data2 = {format: "URL", content: {p1:"Hello", p2:"There"}};
+    const result2 = await nop.func(input(data2), empty_data);
+    assertEquals(result2.output, data2);
 });
+
+Deno.test("define function from full TypeScript definition", async () => {
+  const defined = await define_cmd.func(empty_context, typescript(
+    `import { CommandDefinition, CommandContext, CommandData } from "https://raw.githubusercontent.com/curtcox/CommandInterpreter/main/CommandDefinition.ts";
+
+    export const command: CommandDefinition = {
+        meta: {
+          name: "nop",
+          doc: "typescript nop",
+          source: import.meta.url,
+          input_formats: [],
+          output_formats: []
+        },
+        func: (context: CommandContext, _options: CommandData) => {
+          return Promise.resolve({
+            commands: context.commands,
+            output: context.input
+          });
+        }
+      };
+  `));
+  const out = defined.output.content.meta;
+  assertEquals(out.name, "nop");
+  assertEquals(out.input_formats, []);
+  assertEquals(out.output_formats, []);
+
+  const commands = defined.commands;
+  const nop = commands["nop"];
+  const meta = nop.meta;
+  assertEquals(meta.name, "nop");
+  assertEquals(meta.doc, "typescript nop");
+  assertEquals(meta.input_formats, []);
+  assertEquals(meta.output_formats, []);
+
+  const data1 = {format: "URL", content: new URL(import.meta.url)};
+  const result1 = await nop.func(input(data1), empty_data);
+  assertEquals(result1.output, data1);
+
+  const data2 = {format: "URL", content: {p1:"Hello", p2:"There"}};
+  const result2 = await nop.func(input(data2), empty_data);
+  assertEquals(result2.output, data2);
+});
+
+
+// Deno.test("define function from options using arrow function", async () => {
+//     const defined = await define_cmd.func(empty_context, javascript("add: (x: number, y: number) => x + y"));
+//     assertEquals(defined.output.content, "defined add");
+
+//     const commands = defined.commands;
+//     const add = commands["add"];
+//     assertEquals(add.meta.name, "add");
+//     assertEquals(add.meta.input_formats, ["Object","JSON"]);
+
+//     const json = await add.func(empty_context, { format: "JSON", content: '{"x":10, "y":2}' });
+//     assertEquals(json.output.content, 12);
+
+//     const obj = await add.func(empty_context, { format: "object", content: {x:10, y:2} });
+//     assertEquals(obj.output.content, 12);
+// });
+
+// Deno.test("define function from options using function declaration", async () => {
+//     const defined = await define_cmd.func(empty_context, javascript("function cat(p1, p2) { return p1 + p2; }"));
+//     assertEquals(defined.output.content, "defined cat");
+
+//     const commands = defined.commands;
+//     const cat = commands["cat"];
+//     assertEquals(cat.meta.name, "cat");
+//     assertEquals(cat.meta.input_formats, ["JSON"]);
+    
+//     const json = await cat.func(empty_context, { format: "JSON", content: '{"p1":"Hey", "p2":"You"}' });
+//     assertEquals(json.output.content, "HeyYou");
+
+//     const obj = await cat.func(empty_context, { format: "object", content: {p1:"Hello", p2:"There"} });
+//     assertEquals(obj.output.content, "HelloThere");
+// });
+
+// Deno.test("define function from options using function expression", async () => {
+//     const defined = await define_cmd.func(empty_context, javascript("const rgb = function(r, g, b) { return `rgb(${r},${g},${b})`; }"));
+//     assertEquals(defined.output.content, "defined rgb");
+
+//     const commands = defined.commands;
+//     const rgb = commands["rgb"];
+//     assertEquals(rgb.meta.name, "rgb");
+//     assertEquals(rgb.meta.input_formats, ["JSON"]);
+    
+//     const json = await rgb.func(empty_context, { format: "JSON", content: '{"r":10, "g":4, "b":2}' });
+//     assertEquals(json.output.content, "rgb(10,4,2)");
+
+//     const obj = await rgb.func(empty_context, { format: "object", content: {r:3, g:6, b:9} });
+//     assertEquals(obj.output.content, "rgb(3,6,9)");
+// });
+
+// Deno.test("define function from input using arrow function", async () => {
+//     const defined = await define_cmd.func(empty_context, javascript("multiply: (a: number, b: number) => a * b"));
+//     assertEquals(defined.output.content, "defined multiply");
+
+//     const commands = defined.commands;
+//     const multiply = commands["multiply"];
+//     assertEquals(multiply.meta.name, "multiply");
+//     assertEquals(multiply.meta.input_formats, ["JSON"]);
+    
+//     const json = await multiply.func(empty_context, { format: "JSON", content: '{"a":10, "b":2 }' });
+//     assertEquals(json.output.content, 20);
+
+//     const obj = await multiply.func(empty_context, { format: "object", content: {a:3, b:9} });
+//     assertEquals(obj.output.content, 27);
+// });
+
+// Deno.test("define function from input using function declaration", async () => {
+//     const f = `
+//     function dup(times: number) {
+//         let out = "";
+//         for (let i = 0; i < times; i++) {
+//             out += input;
+//         }
+//         return out;
+//     }`;
+//     const defined = await define_cmd.func(context_with_func(f), javascript(""));
+//     const commands = defined.commands;
+//     const dup = commands["dup"];
+//     assertEquals(dup.meta.name, "dup");
+//     assertEquals(dup.meta.input_formats, ["JSON"]);
+
+//     const json = await dup.func(context_with_func("root"), { format: "JSON", content: '{"times":3}' });
+//     assertEquals(json.output.content, "rootrootroot");
+
+//     const obj = await dup.func(context_with_func("Run"), { format: "JSON", content: '{"times":4}' });
+//     assertEquals(json.output.content, "RunRunRunRun");
+// });
