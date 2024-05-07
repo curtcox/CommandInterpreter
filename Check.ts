ba356b89d01bd53999696a944ee037093e62c5be
Curt Cox <curtcox@gmail.com>
2024-05-07 09:52:18 -0500

    Doc.

--- Check.ts
+++ Check.ts
@@ -1,3 +1,6 @@
+/**
+ * Check that this thing is defined, because we really need it to be defined, OK? 
+ */
 export function check<T>(value: T) : T {
     if (!value) {
       throw new Error("Value is undefined");
