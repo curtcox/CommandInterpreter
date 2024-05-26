bd33152d50963fd7a1cbaee06839da3987f438a6
Curt Cox <curtcox@gmail.com>
2024-05-26 09:28:56 -0500

    Test that store command can load and save command records and errors.

--- core_commands/StoreCommand_test.ts
+++ core_commands/StoreCommand_test.ts
@@ -1,11 +1,16 @@
-import { assertEquals } from "https://deno.land/std@0.223.0/assert/mod.ts";
-import { CommandContext, CommandData, CommandDefinition } from "../command/CommandDefinition.ts";
+import { assertEquals, assert } from "https://deno.land/std@0.223.0/assert/mod.ts";
+import { CommandContext, CommandData, CommandDefinition, CommandRecord } from "../command/CommandDefinition.ts";
 import { memory, store_cmd, get, set } from "./StoreCommand.ts";
 import { invoke, invoke_with_input } from "../command/ToolsForCommandWriters.ts";
 import { STORE } from "../command/CommandDefinition.ts";
-import { emptyContextMeta } from "../command/Empty.ts";
+import { emptyContextMeta, emptyData } from "../command/Empty.ts";
+import { are_equal } from "../Objects.ts";
+import { Duration } from "../command/CommandDefinition.ts";
+import { PreciseTime } from "../command/CommandDefinition.ts";
+import { CommandResult } from "../command/CommandDefinition.ts";
+import { nop_cmd } from "./NopCommand.ts";
 
-const empty = {format:"", content:""};
+const empty = emptyData;
 
 const contextWithStore = (store: CommandDefinition) : CommandContext => ({
   commands: {"store": store},
@@ -94,4 +99,50 @@ Deno.test("Set value can be obtained via get using convenience functions", async
   await set(context, "bar", value);
   const result = await get(context, "bar");
   assertEquals(result, value);
+});
+
+function now_now(): PreciseTime {
+  return { millis: Date.now(), micros: performance.now() };
+}
+
+Deno.test("Empty command record can be loaded and saved from memory store", async () => {
+  const store = store_cmd(memory());
+  const context = contextWithStore(store);
+  const id = 1;
+  const command = nop_cmd;
+  const options = empty;
+  const result: CommandResult = {
+    commands: {},
+    output: empty,
+  }
+  const duration: Duration = {
+     start: now_now(), end: now_now()
+  }
+  const record: CommandRecord = {
+    id, command, options, context, result, duration
+  }
+  const from_store = await save_and_load(context, {format: "CommandRecord", content: record});
+  const loaded = from_store.content as CommandRecord;
+  assert(are_equal(loaded, record));
+});
+
+async function save_and_load(context: CommandContext, value: CommandData) : Promise<CommandData> {
+  const key = "x";
+  set(context, key, value);
+  const data = await get(context, key);
+  assertEquals(data.format, value.format);
+  return data;
+}
+
+Deno.test("error can be loaded and saved from memory store", async () => {
+  const store = store_cmd(memory());
+  const context = contextWithStore(store);
+
+  const error = new Error("This is an error.");
+  const from_store = await save_and_load(context, {format: "Error", content: error});
+  const loaded = from_store.content as Error;
+  assertEquals(loaded.name, error.name);
+  assertEquals(loaded.message, error.message);
+  assertEquals(loaded.stack, error.stack);
+  assert(are_equal(loaded, error));
 });
