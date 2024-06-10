764fbce1cc6f8b8cb4eb13823edbbdd31720a492
Curt Cox <curtcox@gmail.com>
2024-06-10 14:26:53 -0500

    Implementing ref command.

---
+++ core_commands/RefCommand.ts
@@ -0,0 +1,67 @@
+import { hash } from "./HashCommand.ts";
+import { use_replacement } from "../Strings.ts";
+
+
+export interface Ref {
+    result: string;
+    replacements: Map<string, string>; // Map<hash, subtree>
+}
+
+const cutoff = 88;
+
+function size(value: unknown): number {
+    if (typeof value === "string") {
+        return value.length;
+    }
+    return JSON.stringify(value).length;
+}
+
+const big_enough_to_hash     = (value: unknown): boolean => size(value) > cutoff;
+const small_enough_to_return = (value: unknown): boolean => size(value) <= cutoff;
+const stringify              = (value: unknown): string => JSON.stringify(value);
+
+export async function jsonToRef(json: string): Promise<Ref> {
+    const replacements: Map<string, string> = new Map();
+    if (small_enough_to_return(json)) {
+        return {result: json, replacements};
+    }
+    const o = JSON.parse(json);
+    for (const key in o) {
+        const value = o[key];
+        if (big_enough_to_hash(value)) {
+            const ref = jsonToRef(stringify(value));
+            o[key] = (await ref).result;
+            for (const [hash, subtree] of (await ref).replacements.entries()) {
+                replacements.set(hash, subtree);
+            }
+        }
+    }
+    const out = stringify(o);
+    const result = await hash(out);
+    replacements.set(result, out);
+    return { result, replacements };
+}
+
+export function refToJson(ref: Ref): string {
+    const replacements = ref.replacements;
+
+    function resolveHashes(str: string): string {
+        let resolved = str;
+        for (const [hash, subtree] of replacements.entries()) {
+            resolved = use_replacement(resolved, {key: `"${hash}"`, value: subtree});
+            resolved = use_replacement(resolved, {key: hash, value: subtree});
+        }
+        return resolved;
+    }
+
+    let json = ref.result;
+
+    // Continuously replace hashes with subtrees until no hashes are left
+    let previous;
+    do {
+        previous = json;
+        json = resolveHashes(json);
+    } while (json !== previous);
+
+    return json;
+}
---
+++ core_commands/RefCommand_test.ts
@@ -0,0 +1,93 @@
+import { jsonToRef, refToJson, Ref } from "./RefCommand.ts";
+import { hash } from "./HashCommand.ts";
+import { assertEquals } from "https://deno.land/std@0.223.0/assert/mod.ts";
+
+function assertValid(original: string, result: Ref) {
+    // assertEquals(result.result, original);
+    // console.log({result, original});
+    const normal = JSON.stringify(JSON.parse(original));
+    assertEquals(refToJson(result), normal);
+}
+
+Deno.test("Empty object", async () => {
+    const input = "{}";
+    const result = await jsonToRef(input);
+    assertValid(input, result);
+});
+
+Deno.test("Simple object", async () => {
+  const input = '{"name":"John","age":30}';
+  const result = await jsonToRef(input);
+  assertEquals(result.replacements.size, 0);
+  assertValid(input, result);
+});
+
+Deno.test("Simple object -- when too large", async () => {
+    const input = '{"name of the thing that you know":"John John the Baptist son","age at the time when he did that thing with the thing":30}';
+    const result = await jsonToRef(input);
+    assertEquals(result.replacements.size, 1);
+    assertEquals(result.result, 'eB2Nk/hGO8cyCR2zYP6JchcoCJVsL5ReVF020WAB91SFWY4CksOYi1MYex9gEvrNjeg23jyOCOXajDEcLwancA==');
+    assertValid(input, result);
+});
+
+Deno.test("Object with nested objects", async () => {
+  const input = `{"name":"John","age":30,"address":{"street":"123 Main St","city":"New York"}}`;
+  const result = await jsonToRef(input);
+  assertEquals(result.replacements.size, 0);
+  assertValid(input, result);
+});
+
+Deno.test("Object with nested objects -- when too large", async () => {
+    const input = `{"name of the rose":"John, John Rose","age":300456,"address me by my name":{"street":"123 Main Street","city":"New York"}}`;
+    const result = await jsonToRef(input);
+    assertEquals(result.replacements.size, 1);
+    assertEquals(result.result, 'lHk8dPk2I+HmeROdxMWaQlhW8Ug+E1ByCfN8a9QOtl96M0dU6WjBzgkbgwQD6dRkSXFGTAWmrrL+3df6B589/Q==');
+    assertValid(input, result);
+});
+
+Deno.test("Object with arrays", async () => {
+    const input = `{"name":"J","age":9,"eh":["take","off","your","shoes"]}`;
+    const result = await jsonToRef(input);
+    assertEquals(result.replacements.size, 0);
+    assertEquals(result.result, input);
+    assertValid(input, result);
+  });
+
+Deno.test("Object with arrays -- when too large", async () => {
+  const input = `{"first name":"John","age":30,"hobbies and stuff":["reading the things that I read","traveling the places that I go"]}`;
+  const result = await jsonToRef(input);
+  assertEquals(result.replacements.size, 1);
+  assertEquals(result.result, '33zu/lG6NKogHTf5MeVdfM+fsHEKEGHC1q98+VlsJjoYWro2OoX6iVudiwNaIf/SaFeq2AXYYWVY0AwwMK6rjw==');
+  assertValid(input, result);
+});
+
+Deno.test("Complex object", async () => {
+  const input = `{
+    "name": "John",
+    "age": 30,
+    "married": false,
+    "hobbies": ["reading", "traveling"],
+    "address": {
+      "street": "123 Main St",
+      "city": "New York",
+      "country": "USA"
+    },
+    "education": [
+      {
+        "degree": "Bachelor's",
+        "major": "Computer Science",
+        "university": "ABC University",
+        "notes": "That by their existence alone make this object larger than 88 characters -- and thus require hashing. This is a test of the hashing function."
+      },
+      {
+        "degree": "Master's",
+        "major": "Data Science",
+        "university": "XYZ University"
+      }
+    ]
+  }`;
+  const result = await jsonToRef(input);
+  assertEquals(result.replacements.size, 4);
+  assertEquals(result.result, "2CZ5t2f+E7uAnIzm8DFRKYXYc4nDp605YqyOAwbHYvTFfE2wAWJSGnIXsB6q24sp5ESjyoBSFukFgUR2dbPapA==");
+  assertValid(input, result);
+});
